e44568726212eb69247f899646fd95f7edcbd591
TemmaVN <temmavn@gmail.com>
2025-02-13 08:12:27 +0700

    initialcommit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+﻿void Myfirstmethod()
+{
+
+}
+
+void Mysecondmethod()
+{
+
+}
+
+void Mythirdmethod()
+{
+
+}
