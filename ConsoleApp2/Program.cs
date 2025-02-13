0b88f800138c79ee8fd171a6c8dcc2629cd71aa2
TemmaVN <temmavn@gmail.com>
2025-02-13 08:06:34 +0700

    newbie

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
+Console.WriteLine("Hello, World!,+1 modifiel,one mprw time" + "");
